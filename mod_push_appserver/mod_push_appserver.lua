7176e82d3d6d4fe65f3f618fcde3f05199bc5379
Thilo Molitor <thilo@eightysoft.de>
2020-09-03 16:59:53 +0200

    Fix wrong token2node cache invalidation, should fix #28

--- mod_push_appserver/mod_push_appserver.lua
+++ mod_push_appserver/mod_push_appserver.lua
@@ -64,12 +64,15 @@ local push_store = (function()
 			end
 		end
 		if not cache[node] then cache[node] = {} end
+		-- add entry to token2node cache, too
+		if cache[node].token and cache[node].node then token2node_cache[cache[node].token] = cache[node].node; end
 		return cache[node], true;
 	end
 	function api:set(node, data)
 		local settings = api:get(node);		-- load node's data
-		if settings.token then token2node_cache[settings.token] = nil; end		-- invalidate token2node cache
+		-- fill caches
 		cache[node] = data;
+		if settings.token and settings.node then token2node_cache[settings.token] = settings.node; end
 		local ok, err = store:set(node, cache[node]);
 		if not ok then
 			module:log("error", "Error writing push notification storage for node '%s': %s", node, tostring(err));
